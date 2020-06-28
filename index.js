164ff31015e4ab400c2e5fd2be2acad650f9f344
mateus.coradini <mateus.coradini@dextra-sw.com>
2020-06-28 18:18:37 -0300

    Dev changes

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const keys = require('./config/keys');
 require('./models/User');
 require('./services/passport');
 
-mongoose.connect(keys.mongoURI);
+mongoose.connect(keys.mongoURI, {useNewUrlParser: true });
 
 const app = express();
 
