7f6cd941b14298bc327e68d11b24e1ebec35dd7c
mateus.coradini <mateus.coradini@dextra-sw.com>
2021-02-11 17:48:07 -0300

    OAuth

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,46 +1,37 @@
-import React from 'react';
-
+import React, { Component }  from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
+import { connect } from 'react-redux';
+import * as actions from '../actions';
 
+import Header from './Header';
+import Landing from './Landing';
 
-
-const Header = () => <h2>Header</h2>
 const Dashboard = () => <h2>Dashboard</h2>
 const SurveyNew = () => <h2>SurveyNew</h2>
-const Landing = () => <h2>Landing</h2>
 
-const App = () => {
-  return(
-    <div>
-      <BrowserRouter>
-      <div>
-        <Route path="/" component={Landing}/>
-        <Route path="/surveys" component={Dashboard}/>
+
+class App extends Component {
+
+  componentDidMount() {
+    this.props.fetchUser();
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <BrowserRouter>
+          <div>
+            <Header />
+            <Route exact path="/" component={Landing} />
+            <Route exact path="/surveys" component={Dashboard} />
+            <Route path="/surveys/new" component={SurveyNew} />
+          </div>
+
+        </BrowserRouter>
       </div>
-      
-      </BrowserRouter>
-    </div>
-  );
-
+    );
+  }
 };
-// const Header = () => {
-//   <h2> Header</h2>;
-// };
 
-// const Dashboard = () => {
-//   <h2> Dashboard</h2>;
-// };
 
-// const SurveyNew = () => {
-//   <h2> SurveyNew</h2>;
-// };
-
-// const Landing = () => {
-//   <h2> Landing</h2>;
-// };
-
-// const App = () => {
-//   return <div>Hi There!</div>;
-// };
-
-export default App;
+export default connect(null, actions)(App);
---
+++ client/src/components/Landing.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+const Landing = () => {
+
+    return (
+        <div style={{textAlign: 'center'}}>
+            <h1>
+                Emaily!
+            </h1>
+            Coletando feedback de seus usuarios.
+        </div>
+    );
+
+};
+
+export default Landing;
